21ac3e6c3ed706765d073b38806d66b7637f66dd
SoU-240517 <fsttamaprot.broadside024@passinbox.com>
2025-07-18 15:22:08 +0900

    git のキャッシュクリア

--- fractal_editor/ui/__init__.py
+++
@@ -1,9 +0,0 @@
-# UI components package
-
-from .main_window import MainWindow
-from .fractal_widget import FractalWidget
-from .parameter_panel import ParameterPanel
-from .formula_editor import FormulaEditorWidget
-from .export_dialog import ImageExportDialog
-
-__all__ = ['MainWindow', 'FractalWidget', 'ParameterPanel', 'FormulaEditorWidget', 'ImageExportDialog']
